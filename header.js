7dac9ffba0236feb3ebc6d7d47d547b4fb72cdd8
Nicolò Spampatti <nicolo.spampa@gmail.com>
2023-05-05 11:28:24 +0200

    fixed warnings

--- header.js
+++ header.js
@@ -5,10 +5,12 @@ const inMenuBurger = document.getElementById('inMenuBurger')
 burger.addEventListener('click', () => {
   header.classList.remove('invisible');
   inMenuBurger.classList.remove('hidden');
+  inMenuBurger.classList.add('flex');
 });
 
 inMenuBurger.children[0].addEventListener('click', () => {
   header.classList.add('invisible');
+  inMenuBurger.classList.remove('flex');
   inMenuBurger.classList.add('hidden');
   console.log("premuto");
 });
